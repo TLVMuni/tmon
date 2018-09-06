ce617085fde4ff6c7bf7fed428be924b089e8f64
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-06 19:51:02 +0300

    Update Dashboard.tsx

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import Sidebar from './Sidebar';
 
 class Dashboard extends React.Component {
     render() {
-        return <div>
+        return <div className='wrapper'>
+            <Sidebar {...this.props}/>
             Dashboard
         </div>
     }
