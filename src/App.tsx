e5ff814396272794cbd22480f0353638b83129a1
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-10 21:40:46 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import UserInterface from './UserInterface';
-
 import Dashboard from './Dashboard';
 
-class App extends React.Component<UserInterface, any> {
-
-    render() {
-
-        return <Switch>
-            <Route exact path='/' component={Dashboard} />
-        </Switch>
-    }
+const App = (props) => {
+    return <Switch>
+        <Route path='/' component={Dashboard} />
+    </Switch>
 }
 
 export default App;
