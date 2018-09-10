56641f46eea806af101c6f7e03962e63d013ea95
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-10 21:43:09 +0300

    Update Dashboard.tsx

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import Header from './Header';
+import Home from './Home';
+import Region from './Region';
 
 class Dashboard extends React.Component {
     render() {
         return <div className='wrapper'>
             <Sidebar {...this.props}/>
-            Dashboard
+            <div className="main-panel" ref="mainPanel">
+                <Header {...this.props}/>
+                <Switch>
+                    <Route exact path='/home' render={ () => <Home />} />
+                    <Route path={this.props.match.path + 'region/:regionId'} component={Region} />
+                </Switch>
+            </div>
         </div>
     }
 }
