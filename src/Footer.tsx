c2dbe21c4ad8a45e65bf780f6b46cdff50ffecf1
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-11 02:09:03 +0300

    Add files via upload

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,18 +1,32 @@
+
 import * as React from 'react';
 
-class Footer extends React.Component<FooterProps> {
+class Footer extends React.Component<FooterProps, {}> {
 
     render() {
 
-        const cameras = this.props.cameras.map( (camera, index) => {
-            return <div key={index}>{camera.id}</div>
+        const cameras = this.props.cameras.map( (camera: ICamera, index: number) => {
+            return <li key={index}>
+                      <div className='carousel-item-div selected'>{camera.id}</div>
+                    </li>
         });
 
         return <div id='footer'>
             <div id='footerDesktop'>
                 <div className='carousel'>
                     <div className='carouselWrapper'>
-                    {cameras}
+                        <div className='carouselScroller no-touch'
+                            style={{
+                                width: '2340px',
+                                transitionProperty: 'transform',
+                                transformOrigin: '0px 0px 0px',
+                                transform: 'translate(0px, 0px) translateZ(0px)'
+                            }}>
+                            <ul>
+                                {cameras}
+                            </ul>
+                            
+                        </div>                    
                     </div>
                 </div>
             </div>
