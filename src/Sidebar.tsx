ba2be8cc1e6a493113e9e70b9eab61b05efe9e68
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-10 21:42:49 +0300

    Update Sidebar.tsx

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,10 +1,44 @@
+
 import * as React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 class Sidebar extends React.Component {
+
+    state = {
+        sideBarOpen: true
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.onSetSidedabarOpen = this.onSetSidedabarOpen.bind(this);
+    }
+
+    onSetSidedabarOpen(open) {
+        this.setState({
+            sideBarOpen: !this.state.sideBarOpen
+        })
+    }
+
     render() {
-        return <div className='sidebar'>
-            Sidebar
-        </div>
+        return (
+            <nav id='sidebar'>
+                <div className='sidebar-header'>
+                    <h3>TLV Traffic Monitor</h3>
+                </div>
+                <ul className="list-unstyled components">
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/region/1'>Region1</Link>
+                    </li>
+                    <li>
+                        <Link to='/region/2'>Region2</Link>
+                    </li>                    
+                </ul>
+            </nav>
+        )
     }
 };
 
