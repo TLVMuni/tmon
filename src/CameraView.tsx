e3d5c07766e59e7f294e2b9a5578349556e34702
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-11 16:21:50 +0300

    Add files via upload

---
+++ src/CameraView.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+class CameraView extends React.Component<CameraViewProps> {
+
+    render() {
+        return <div id='picturePanel'>
+            <div id='cfader'>
+                <div className='member-image current'>
+                    <iframe width="300" height="200" style={{border:'0'}} webkitallowfullscreen="true" 
+                        scrolling="no" mozallowfullscreen="true" allowFullScreen={true} 
+                        src="https://balticlivecam.com/cameras/israel/tel-aviv/tel-aviv/?embed">
+                    </iframe>
+                </div> 
+                <div id='placardContainer'>
+                    <div id='toggle' style={{
+                        outline: 'none'
+                    }}>
+                    </div>
+                </div>
+            </div>
+            CameraView: {this.props.cameraId}
+        </div>
+    }
+
+};
+
+export default CameraView;
