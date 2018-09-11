1cb30ece45c9487d259065f44158b0db446cae72
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-11 16:21:27 +0300

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,13 +1,51 @@
 
 import * as React from 'react';
+import * as noPicture from './nopicture.png';
+import classNames from 'classnames';
 
-class Footer extends React.Component<FooterProps, {}> {
+class Footer extends React.Component<FooterProps, FooterState> {
+
+    state = {
+        activeCameraId: this.props.cameras[0].id
+    }
+
+    cameraClicked = (cameraId: number) => {
+        this.setState({
+            activeCameraId: cameraId
+        });
+
+        this.props.cameraSelected(cameraId);
+    }
+
+    componentDidUpdate(prevProps: FooterProps) {
+        if( prevProps.cameras !== this.props.cameras ) {
+            this.setState({
+                activeCameraId: this.props.cameras[0].id
+            })
+        };
+    }
 
     render() {
 
+        const self = this;
+
         const cameras = this.props.cameras.map( (camera: ICamera, index: number) => {
+
+            let cameraClassName = classNames({
+                'carousel-item-div': true,
+                'activeCamera': self.state.activeCameraId === camera.id
+            });
+    
             return <li key={index}>
-                      <div className='carousel-item-div selected'>{camera.id}</div>
+                      <div className={cameraClassName} tab-index='0'
+                        onClick={ () => this.cameraClicked(camera.id)}>
+                        <span className='number-red'>{camera.id}</span>
+                        <img src={'public/' + noPicture} alt="picture not availbale" />
+                        <div>{camera.name}</div>    
+                        <div style={{height: 0}}>.</div>
+                        <div style={{height: 0}}>.</div>
+                        <div style={{height: 0}}>View full screen</div>                    
+                      </div>
                     </li>
         });
 
