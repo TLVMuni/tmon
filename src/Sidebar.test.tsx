6a8c4d29fc02bff225962a0aa397ab99a771b5a6
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-12 00:53:53 +0300

    Add files via upload

---
+++ src/Sidebar.test.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { configure, shallow } from 'enzyme';
+//import * as Adapter from 'enzyme-adapter-react-16';
+import { Sidebar } from './Sidebar';
+
+//configure({ adapter: new Adapter() });
+
+// See more: https://basarat.gitbooks.io/typescript/docs/testing/jest.html
+
+test('Initial Sidebar render', () => {
+
+    const sidebar = shallow(<Sidebar  />);
+    
+    // Interaction demo 
+    expect(sidebar.text()).toEqual('Off');
+    sidebar.find('input').simulate('change');
+    expect(sidebar.text()).toEqual('On');
+
+    // Snapshot demo
+    expect(sidebar).toMatchSnapshot();
+});
