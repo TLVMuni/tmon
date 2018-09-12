9dbd39327b950c4eb0d014ac264530075020f836
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-09-12 03:15:53 +0300

    Update CameraView.tsx

--- src/CameraView.tsx
+++ src/CameraView.tsx
@@ -6,8 +6,8 @@ class CameraView extends React.Component<CameraViewProps> {
         return <div id='picturePanel'>
             <div id='cfader'>
                 <div className='member-image current'>
-                    <iframe width="300" height="200" style={{border:'0'}} webkitallowfullscreen="true" 
-                        scrolling="no" mozallowfullscreen="true" allowFullScreen={true} 
+                    <iframe width="300" height="200" style={{border:'0'}} 
+                        scrolling="no" allowFullScreen={true} 
                         src="https://balticlivecam.com/cameras/israel/tel-aviv/tel-aviv/?embed">
                     </iframe>
                 </div> 
